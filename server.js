38349dca7b1ec826b787709dbad6f5a1b3c15335
l1v1njusttodie <97668472+l1v1njusttodie@users.noreply.github.com>
2022-04-21 10:14:27 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use("/", require("./routes/index"));
 app.use("/basket", require("./routes/basket"));
